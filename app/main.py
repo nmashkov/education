eb872e828e53e0009ac67ea4a3f08b9ea4636026
Nikita Mashkov <nmashkov1997@ya.ru>
2024-04-11 10:53:46 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ async def global_exception_handler(request: Request, exc: Exception):
     """
     return JSONResponse(
         status_code=500,
-        content={"error": "ЛУЛУЛУЛ Internal server error"}
+        content={"error": "Internal server error"}
     )
 
 
